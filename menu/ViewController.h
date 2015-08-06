e3d01aea436d8da9ddea53fbc8c7369ab316dd19
Water <water@WaterdeMacBook.local>
2015-08-06 09:16:06 +0800

    Initial Commit

---
+++ menu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  menu
+//
+//  Created by Water on 15/8/6.
+//  Copyright (c) 2015年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ menu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  menu
+//
+//  Created by Water on 15/8/6.
+//  Copyright (c) 2015年 water. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
